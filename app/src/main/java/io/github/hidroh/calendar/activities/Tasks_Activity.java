e0bd1cc4f413e705e22f1a171bd088cfd3fed860
zahid.ali <zahid.r@students.amikom.ac.id>
2017-10-31 08:45:23 +0700

    Tasks Search

--- app/src/main/java/io/github/hidroh/calendar/activities/Tasks_Activity.java
+++ app/src/main/java/io/github/hidroh/calendar/activities/Tasks_Activity.java
@@ -2,13 +2,21 @@ package io.github.hidroh.calendar.activities;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.BaseColumns;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -25,6 +33,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -36,17 +51,24 @@ import jp.co.recruit_lifestyle.android.widget.WaveSwipeRefreshLayout;
 public class Tasks_Activity extends AppCompatActivity implements ListView.OnItemClickListener, WaveSwipeRefreshLayout.OnRefreshListener{
 
     private ListView listView;
-
     private String JSON_STRING;
-
     private WaveSwipeRefreshLayout mWaveSwipeRefreshLayout;
-
     private android.support.design.widget.FloatingActionButton fab;
+
+
+    // CONNECTION_TIMEOUT and READ_TIMEOUT are in milliseconds
+    public static final int CONNECTION_TIMEOUT = 10000;
+    public static final int READ_TIMEOUT = 15000;
+    private SimpleCursorAdapter myAdapter;
+    SearchView searchView = null;
+    private String[] strArrData = {"No Suggestions"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_tasks_ol_v2);
+
         listView = (ListView) findViewById(R.id.listView);
         listView.setOnItemClickListener(this);
         getJSON();
@@ -61,7 +83,20 @@ public class Tasks_Activity extends AppCompatActivity implements ListView.OnItem
                 startActivity(intent);
             }
         });
+
+
+
+
+        final String[] from = new String[] {"subject_tasks"};
+        final int[] to = new int[] {android.R.id.text1};
+
+        // setup SimpleCursorAdapter
+        myAdapter = new SimpleCursorAdapter(Tasks_Activity.this, android.R.layout.simple_spinner_dropdown_item, null, from, to, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+
+        // Fetch data from mysql table using AsyncTask
+        new AsyncFetch().execute();
     }
+
 
     private void initView() {
         mWaveSwipeRefreshLayout = (WaveSwipeRefreshLayout) findViewById(R.id.main_swipe);
@@ -157,6 +192,7 @@ public class Tasks_Activity extends AppCompatActivity implements ListView.OnItem
     }
 
 
+
     private void refresh(){
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -177,10 +213,59 @@ public class Tasks_Activity extends AppCompatActivity implements ListView.OnItem
         refresh();
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_task, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchManager searchManager = (SearchManager) Tasks_Activity.this.getSystemService(Context.SEARCH_SERVICE);
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(Tasks_Activity.this.getComponentName()));
+            searchView.setIconified(false);
+            searchView.setSuggestionsAdapter(myAdapter);
+            // Getting selected (clicked) item suggestion
+            searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
+                @Override
+                public boolean onSuggestionClick(int position) {
+
+                    // Add clicked text to search box
+                    CursorAdapter ca = searchView.getSuggestionsAdapter();
+                    Cursor cursor = ca.getCursor();
+                    cursor.moveToPosition(position);
+                    searchView.setQuery(cursor.getString(cursor.getColumnIndex("subject_tasks")),false);
+                    return true;
+                }
+
+                @Override
+                public boolean onSuggestionSelect(int position) {
+                    return true;
+                }
+            });
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String s) {
+
+                    // Filter data
+                    final MatrixCursor mc = new MatrixCursor(new String[]{ BaseColumns._ID, "subject_tasks" });
+                    for (int i=0; i<strArrData.length; i++) {
+                        if (strArrData[i].toLowerCase().startsWith(s.toLowerCase()))
+                            mc.addRow(new Object[] {i, strArrData[i]});
+                    }
+                    myAdapter.changeCursor(mc);
+                    return false;
+                }
+            });
+        }
         return true;
     }
 
@@ -199,12 +284,149 @@ public class Tasks_Activity extends AppCompatActivity implements ListView.OnItem
         } else if(id == R.id.btnDraft){
             Intent myAplikasi3 = new Intent(this,Tasks_List_Draft_Activity.class);
             startActivity(myAplikasi3);
-            return true;
+//            return true;
         }
 
         return super.onOptionsItemSelected(item);
 
     }
+
+
+    // Every time when you press search button on keypad an Activity is recreated which in turn calls this function
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            if (searchView != null) {
+                searchView.clearFocus();
+            }
+
+            // User entered text and pressed search button. Perform task ex: fetching data from database and display
+
+        }
+    }
+
+    // Create class AsyncFetch
+    private class AsyncFetch extends AsyncTask<String, String, String> {
+
+        ProgressDialog pdLoading = new ProgressDialog(Tasks_Activity.this);
+        HttpURLConnection conn;
+        URL url = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
+
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+
+                // Enter URL address where your php file resides or your JSON file address
+                url = new URL("http://128.159.1.65/PHP/fetch-all-fish.php");
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return e.toString();
+            }
+            try {
+
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("GET");
+
+                // setDoOutput to true as we receive data
+                conn.setDoOutput(true);
+                conn.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return e1.toString();
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+
+                } else {
+                    return("Connection error");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.toString();
+            } finally {
+                conn.disconnect();
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //this method will be running on UI thread
+            ArrayList<String> dataList = new ArrayList<String>();
+            pdLoading.dismiss();
+
+
+            if(result.equals("no rows")) {
+
+                // Do some action if no data from database
+
+            }else{
+
+                try {
+
+                    JSONArray jArray = new JSONArray(result);
+
+                    // Extract data from json and store into ArrayList
+                    for (int i = 0; i < jArray.length(); i++) {
+                        JSONObject json_data = jArray.getJSONObject(i);
+                        dataList.add(json_data.getString("subject_tasks"));
+                    }
+
+                    strArrData = dataList.toArray(new String[dataList.size()]);
+
+                } catch (JSONException e) {
+                    // You to understand what actually error is and handle it appropriately
+                    Toast.makeText(Tasks_Activity.this, e.toString(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(Tasks_Activity.this, result.toString(), Toast.LENGTH_LONG).show();
+                }
+
+            }
+
+        }
+
+    }
+
+
     public void keluarYN(){
         AlertDialog.Builder ad=new AlertDialog.Builder(this);
         ad.setTitle("Konfirmasi");
